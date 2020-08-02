745f35c632d2bc00990771b2c8890b41684902bc
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-02 21:56:37 +0900

    単体test

--- db/migrate/20200731102537_add_user_column_to_users.rb
+++ db/migrate/20200731102537_add_user_column_to_users.rb
@@ -1,4 +1,4 @@
-class AddNicknameToUsers < ActiveRecord::Migration[6.0]
+class AddUserColumnToUsers < ActiveRecord::Migration[6.0]
   def change
     # add_column :users, :nickname, :string
     # add_column :users, :first_name, :string
