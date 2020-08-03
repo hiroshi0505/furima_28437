626f968518b968238d7fc74387de1f41b4011df9
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-03 16:45:52 +0900

    Update 20200731102537_add_user_column_to_users.rb

--- db/migrate/20200731102537_add_user_column_to_users.rb
+++ db/migrate/20200731102537_add_user_column_to_users.rb
@@ -1,12 +1,12 @@
 class AddUserColumnToUsers < ActiveRecord::Migration[6.0]
   def change
-    # add_column :users, :nickname, :string
-    # add_column :users, :first_name, :string
-    # add_column :users, :last_name, :string
-    # add_column :users, :first_furigana, :string
-    # add_column :users, :last_furigana, :string
-    # add_column :users, :email, :string
-    # add_column :users, :password, :string
+    add_column :users, :nickname, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :first_furigana, :string
+    add_column :users, :last_furigana, :string
+    add_column :users, :email, :string
+    add_column :users, :password, :string
     add_column :users, :birthday, :date
   end
 end
