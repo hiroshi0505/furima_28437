fe7c71c2959810276c52b8e54ab16000948380d2
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-03 10:15:38 +0900

    単体test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,17 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    # password              {Faker::Internet.password(min_length: 6)}
     password              {"a12345"}
     password_confirmation {password}
-
     first_name            {"森泉"}
     last_name             {"拓志"}
     first_furigana        {"モリズミ"}
     last_furigana         {"ヒロシ"}
     birthday              {Faker::Date.birthday}
-    # nickname              {Faker::Name.initials(number: 2)}
-    # email                 {Faker::Internet.free_email}
-    # password              {Faker::Internet.password(min_length: 6)}
   end
 end
