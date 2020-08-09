73e489b496be09bcd489be7e642b457de887748c
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-09 16:23:54 +0900

    エラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render action: :show
+    end
+
   end
 
   private
