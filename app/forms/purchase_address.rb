461775f17e66c123f6d0ce3a3a21571a2c7a1fc5
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-13 21:25:48 +0900

    単体テストコード記述

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -3,10 +3,13 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :region_id, :city, :street_number, :building_name, :phone_number, :user_id, :item_id, :token
 
+  POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+  PHONE_NUMBER_REGEX = /\A[0-9]{11}\z/
+
   with_options presence: true do  # do~endの全てのカラムに対し、空の選択を保存できないようにする
     validates :city, :street_number
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "can't be blank"}
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "can't be blank"}
+    validates :postal_code, format: {with: POSTAL_CODE_REGEX, message: "can't be blank"}
+    validates :phone_number, format: {with: PHONE_NUMBER_REGEX, message: "can't be blank"}
     validates :region_id, numericality: { other_than: 0, message: "can't be blank" } # 都道府県はid=0が「--」の為
   end
 
